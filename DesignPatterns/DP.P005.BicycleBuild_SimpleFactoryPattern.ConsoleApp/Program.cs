b4073739024de715613d8df6a322bcb2ebcc4e4c
tahirjadoon <tjadoon@gmail.com>
2024-07-11 01:46:58 -0400

    005-BikeBuild-SimpleFactoryPattern

---
+++ DesignPatterns/DP.P005.BicycleBuild_SimpleFactoryPattern.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DP.BicycleBuildBase.Enums;
+using DP.BicycleBuildBase;
+using DP.Core;
+using DP.Core.Extensions;
+using DP.P005.BicycleBuild_SimpleFactoryPattern.ConsoleApp;
+
+ConsoleHelper.TitleWrite("Simple Factory Pattern implementation!");
+
+const string errorText = "You must pass in mountainbike, cruiser, recumbent, or roadbike";
+
+if (args.Length <= 0)
+{
+    ConsoleHelper.ErrorWrite(errorText);
+}
+else
+{
+    var arg = args[0].Trim();
+
+    var bicycleFactory = new SimpleBicycleFactory();
+    var bikeToBuild = bicycleFactory.Create(arg);
+
+    bikeToBuild.Build();
+    ConsoleHelper.WriteEmptyLine();
+    ConsoleHelper.SuccessWrite(bikeToBuild.ToString());
+}
---
+++ DesignPatterns/DP.P005.BicycleBuild_SimpleFactoryPattern.ConsoleApp/SimpleBicycleFactory.cs
@@ -0,0 +1,29 @@
+﻿using DP.BicycleBuildBase;
+using DP.BicycleBuildBase.Enums;
+using DP.Core.Extensions;
+
+namespace DP.P005.BicycleBuild_SimpleFactoryPattern.ConsoleApp;
+
+public class SimpleBicycleFactory
+{
+    public Bicycle Create(string argBicycleType)
+    {
+        var bicycleType = argBicycleType.ToLower().ToEnumNullable<ZBikeTypes>();
+
+        Bicycle bikeToBuild;
+        switch (bicycleType)
+        {
+            case ZBikeTypes.Mountainbike:
+                bikeToBuild = new MountainBike(); break;
+            case ZBikeTypes.Cruiser:
+                bikeToBuild = new CruiserBike(); break;
+            case ZBikeTypes.Recumbent:
+                bikeToBuild = new RecumbentBike(); break;
+            case ZBikeTypes.Roadbike:
+                bikeToBuild = new RoadBike(); break;
+            default:
+                throw new Exception($"Unknown bicycle type: {argBicycleType}");
+        }
+        return bikeToBuild;
+    }
+}
