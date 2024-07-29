eae37d52edde7cf90d7c836754ccaccb9a47bc77
tahirjadoon <tjadoon@gmail.com>
2024-07-28 20:43:39 -0400

    014-BikeBuild-SingletonPattern

---
+++ DesignPatterns/DP.P014.BikeBuild_SingletonPattern.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DP.Core;
+using DP.Core.Extensions;
+using DP.P014.BikeBuild_SingletonPattern.ConsoleApp;
+
+ConsoleHelper.TitleWrite("Sigleton Pattern implementation on top of Object Pool Pattern - Welding Arms!");
+ConsoleHelper.WriteEmptyLine();
+
+//var armPool = new WeldingArmPoolSingleton { MaxSize = 10 };
+var armPool = WeldingArmPoolSingleton.Instance;
+armPool.MaxSize = 10;
+ConsoleHelper.WriteLine($"Pool size: {armPool.ArmsAvailable}", ConsoleColor.Cyan);
+ConsoleHelper.WriteEmptyLine();
+
+var arm01 = armPool.GetArmFromPool();
+ConsoleHelper.WriteLine($"Pool size after picking arm01: {armPool.ArmsAvailable}", ConsoleColor.Cyan);
+arm01.MoveToStation(1);
+ConsoleHelper.WriteLine("arm01 before DoWeld", ConsoleColor.Yellow);
+ConsoleHelper.WriteLine($"\t{arm01.ToJson()}");
+ConsoleHelper.WriteEmptyLine();
+
+var result = arm01.DoWeld();
+ConsoleHelper.WriteLine("arm01 after DoWeld", ConsoleColor.Yellow);
+ConsoleHelper.WriteLine($"\t{arm01.ToJson()}");
+ConsoleHelper.WriteEmptyLine();
+
+if (result)
+{
+    armPool.ReturnArmToPool(arm01);
+}
+
+ConsoleHelper.WriteLine($"Pool size after return: {armPool.ArmsAvailable}", ConsoleColor.Cyan);
+ConsoleHelper.WriteEmptyLine();
---
+++ DesignPatterns/DP.P014.BikeBuild_SingletonPattern.ConsoleApp/WeldingArmPoolSingleton.cs
@@ -0,0 +1,58 @@
+﻿using DP.BicycleBuildBase.Robotics.Arms;
+
+namespace DP.P014.BikeBuild_SingletonPattern.ConsoleApp;
+
+/// <summary>
+/// sealed class and constructor is private
+/// </summary>
+public sealed class WeldingArmPoolSingleton
+{
+    private static readonly Lazy<WeldingArmPoolSingleton> _lazyInstance = new Lazy<WeldingArmPoolSingleton>(() => new WeldingArmPoolSingleton());
+    public static WeldingArmPoolSingleton Instance { get { return _lazyInstance.Value; } }
+
+    private int _maxSize = 10;
+
+    private List<WeldingArm> Pool { get; set; } = null!;
+    public int ArmsAvailable => Pool.Count;
+
+    private WeldingArmPoolSingleton()
+    {
+        Reset();
+    }
+
+    public int MaxSize
+    {
+        get => _maxSize;
+        set
+        {
+            _maxSize = value;
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        Pool = new List<WeldingArm>();
+        for (var i = 0; i < MaxSize; i++)
+        {
+            Pool.Add(new WeldingArm());
+        }
+    }
+
+    public WeldingArm GetArmFromPool()
+    {
+        if (ArmsAvailable > 0)
+        {
+            var returnArm = Pool[0];
+            Pool.RemoveAt(0);
+            return returnArm;
+        }
+        throw new Exception("You are out of arms. Return some to the pool and try again");
+    }
+
+    public void ReturnArmToPool(WeldingArm arm)
+    {
+        arm.CurrentPosition = 0; //not at any station
+        Pool.Add(arm);
+    }
+}
